4e9d221586e600172a33b34822c640b56942b9c3
Alec Down <alecdown@gmail.com>
2018-06-09 11:56:16 -0600

    .

--- app/config/database.js
+++ app/config/database.js
@@ -1,4 +1,16 @@
 module.exports = {
-    database: "mongodb://heroku_0h56k4lb:r4gr6vinug36119baap4hunj3c@ds149960.mlab.com:49960/heroku_0h56k4lb",
-    secret: 'yoursecret'
+  "database": {
+
+    "username": "heroku_0h56k4lb",
+
+    "password": "r4gr6vinug36119baap4hunj3c",
+
+    "database": "heroku_0h56k4lb",
+
+    "host": "ds149960.mlab.com",
+
+    "port": "49960"
+
+  }
+  secret: 'yoursecret'
 }
