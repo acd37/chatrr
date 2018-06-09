46b29d5f251714ac96aff724767cb43a21ddb7c5
Alec Down <alecdown@gmail.com>
2018-06-09 12:50:04 -0600

    adds styling to chat

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,21 +1,11 @@
-
-  $(function () {
-    var socket = io();
-    $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
+$(function () {
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
   });
-
-  $(function () {
-    var socket = io();
-    $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
-    });
+  socket.on('chat message', function(msg){
+    $('#messages').prepend($('<li>').text(msg));
   });
+});
